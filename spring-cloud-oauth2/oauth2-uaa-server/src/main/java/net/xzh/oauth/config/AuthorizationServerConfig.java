e8c34bf037c9920ed91d47f82b0df93daa5af5af
13998417419 <xcg992224@163.com>
2024-05-10 19:55:50 +0800

    add

--- spring-cloud-oauth2/oauth2-uaa-server/src/main/java/net/xzh/oauth/config/AuthorizationServerConfig.java
+++ spring-cloud-oauth2/oauth2-uaa-server/src/main/java/net/xzh/oauth/config/AuthorizationServerConfig.java
@@ -5,52 +5,85 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-/**
- * @Description Oauth2 认证服务配置
- */
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
+	@Autowired
+	private DataSource dataSource;
 
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.allowFormAuthenticationForClients();
-        security.tokenKeyAccess("isAuthenticated()");
-    }
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.jdbc(dataSource);
-    }
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.withClientDetails(jdbcClientDetailsService());
+	}
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.accessTokenConverter(jwtAccessTokenConverter());
-        endpoints.tokenStore(jwtTokenStore());
-    }
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		// token存储到数据库中
+		//endpoints.tokenStore(tokenStore())
+		
+		// 配置Token的存储方式
+		//endpoints.accessTokenConverter(jwtAccessTokenConverter());
+		endpoints.tokenStore(tokenStore())
+				// 注入WebSecurityConfig配置的bean
+				.authenticationManager(authenticationManager);
+	}
 
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		// 对获取Token的请求不再拦截
+		oauthServer.tokenKeyAccess("permitAll()")
+				// 验证获取Token的验证信息
+				.checkTokenAccess("isAuthenticated()")
+				// 这个如果配置支持allowFormAuthenticationForClients的，且对/oauth/token请求的参数中有client_id和client-secret的会走ClientCredentialsTokenEndpointFilter来保护
+				// 如果没有支持allowFormAuthenticationForClients或者有支持但对/oauth/token请求的参数中没有client_id和client_secret的，走basic认证保护
+				.allowFormAuthenticationForClients();
+	}
 
-    @Bean
+	@Bean
     public JwtTokenStore jwtTokenStore() {
         return new JwtTokenStore(jwtAccessTokenConverter());
     }
-
-    @Bean
+	
+	@Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
         jwtAccessTokenConverter.setSigningKey("cjs");   //  Sets the JWT signing key
         return jwtAccessTokenConverter;
     }
+	
+	@Bean
+	public TokenStore tokenStore() {
+		// token存储
+		return new JdbcTokenStore(dataSource);
+	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public ClientDetailsService jdbcClientDetailsService() {
+		// 存储client信息
+		return new JdbcClientDetailsService(dataSource);
+	}
 }
---
+++ spring-cloud-oauth2/oauth2-uaa-server/src/main/java/net/xzh/oauth/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package net.xzh.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import net.xzh.oauth.service.MyUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	/**
+     * 身份验证管理器
+     * 通过自定义实现userDetailsService来实现
+     * 配置了使用BCryptPasswordEncoder哈希来保存用户的密码（生产环境的用户密码肯定不能是明文保存）
+     *
+     * @param auth
+     * @throws Exception
+     */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// 注入userDetailsService的实现类并通过passwordEncoder进行加密
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+	
+	/**
+	 * 静态资源放行
+	 */
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**");
+    }
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin().loginPage("/login").and().authorizeRequests().antMatchers("/login", "/oauth/**").permitAll()
+				.anyRequest().authenticated().and().csrf().disable().cors();
+	}
+}
