fb584a2824a55f05caac96fd2bf62249f68863de
13998417419 <xcg992224@163.com>
2024-07-22 19:23:46 +0800

    add

---
+++ spring-cloud-oauth2/oauth2-web-portal/src/main/java/net/xzh/oauth/WebSSOApplication.java
@@ -0,0 +1,16 @@
+package net.xzh.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 前后端分离的单点登录
+ * @author CR7
+ *
+ */
+@SpringBootApplication
+public class WebSSOApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSSOApplication.class, args);
+    }
+}
